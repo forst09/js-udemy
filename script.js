0deab11f66c0ef10fb26da9c56941aabedfca22c
Daria <dforst09@gmail.com>
2023-05-14 16:00:04 +0300

    19 lesson, 1 practice

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let filmName, filmEstimation;
+for (let i = 0; i < 2; i++) {
+    filmName = prompt('один из последних просмотренных фильмов?');
+    filmEstimation = prompt('на сколько оцените его?');
+    personalMovieDB.movies[filmName] = filmEstimation;
+}
+console.log(personalMovieDB);
+
