5de58418b8eb5ac03cd7de8adde6d30eceda6c61
Дарья <dforst09@gmail.com>
2023-11-02 10:48:32 +0300

    add fetch for food project

--- example/example.js
+++ example/example.js
@@ -1,11 +1,50 @@
 'use strict';
 
-const person = {
-    name: 'alex',
-    tel: '+74444444',
-    parents: {
-        mom: 'olga',
-        dad: 'mike'
-    }
+// console.log('запрос данных...');
+
+// const req = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('подготовка данных');
+
+//         const product = {
+//             name: 'tv',
+//             price: 2000,
+//         };
+
+//         resolve(product);
+//     }, 2000);
+// });
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = 'true';
+//     return data;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('произошла ошибка');
+// }).finally(() => {
+//     console.log('finnaly');
+// })
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    })
 };
-const clone = JSON.parse(JSON.stringify(person));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('all');
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('race');
+});
+
+
