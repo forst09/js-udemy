c6c2c3bea86b36f8de18336d92055c83c176a8d2
Дарья <dforst09@gmail.com>
2023-12-14 14:19:50 +0300

    add pagination for slider

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -273,7 +273,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
             postData('http://localhost:3000/requests', json)
                 .then(data => {
-                    console.log(data);
                     showThanksModal(message.success);
                     statusMessage.remove();
                 }).catch(() => {
@@ -309,7 +308,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     fetch('http://localhost:3000/menu')
         .then(data => data.json())
-        .then(res => console.log(res));
 
     //slider
     const slider = document.querySelector('.offer__slider');
@@ -323,6 +321,16 @@ window.addEventListener('DOMContentLoaded', () => {
     let sliderIndex = 1;
     let offset = 0;
 
+    //make dot active
+    function findSlide(slideIndex) {
+        const dots = document.querySelectorAll('.dot');
+        dots.forEach(dot => {
+            dot.style.opacity = '0.5';
+        })
+        document.querySelector(`.dot[slideindex="${slideIndex}"]`).style.opacity = '1';
+    }
+
+    //write current index
     function insertIndex() {
         if (sliderIndex < 10) {
             sliderNav.querySelector('#current').textContent = `0${sliderIndex}`;
@@ -332,7 +340,69 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //pagination
+    const paginationWrapper = document.createElement('div');
+    paginationWrapper.classList.add('carousel-indicators');
+    paginationWrapper.style.cssText = `
+                                    position: absolute;
+                                    right: 0;
+                                    bottom: 0;
+                                    left: 0;
+                                    z-index: 15;
+                                    display: flex;
+                                    justify-content: center;
+                                    margin-right: 15%;
+                                    margin-left: 15%;
+                                    list-style: none;
+                                `;
+    for (let i = 0; i < slides.length; i++) {
+        let paginationDot = document.createElement('button');
+        paginationDot.classList.add('dot')
+        paginationDot.style.cssText = `
+                                    box-sizing: content-box;
+                                    flex: 0 1 auto;
+                                    width: 30px;
+                                    height: 6px;
+                                    margin-right: 3px;
+                                    margin-left: 3px;
+                                    cursor: pointer;
+                                    background-color: #fff;
+                                    background-clip: padding-box;
+                                    border-top: 10px solid transparent;
+                                    border-bottom: 10px solid transparent;
+                                    border-right: unset;
+                                    border-left: unset;
+                                    opacity: .5;
+                                    transition: opacity .6s ease;
+                                `;
+        paginationDot.setAttribute('slideIndex', i + 1);
+        paginationDot.addEventListener('click', () => {
+            let index = paginationDot.getAttribute('slideIndex'),
+                differenceIndex;
+
+            if (sliderIndex < index) {
+                differenceIndex = index - sliderIndex;
+                offset += +width.slice(0, width.length - 2) * differenceIndex;
+                slidesField.style.transform = `translateX(-${offset}px)`;
+            }
+
+            else if (sliderIndex > index) {
+                differenceIndex = sliderIndex - index;
+                offset -= +width.slice(0, width.length - 2) * differenceIndex;
+                slidesField.style.transform = `translateX(-${offset}px)`;
+            }
+
+            sliderIndex = index;
+            insertIndex();
+
+            findSlide(sliderIndex);
+        })
+        paginationWrapper.append(paginationDot);
+    }
+    slider.append(paginationWrapper);
+
     insertIndex();
+    findSlide(sliderIndex);
 
     if (slides.length < 10) {
         sliderNav.querySelector('#total').textContent = `0${slides.length}`;
@@ -371,6 +441,8 @@ window.addEventListener('DOMContentLoaded', () => {
             offset += +width.slice(0, width.length - 2);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
+
+        findSlide(sliderIndex);
     });
 
     sliderPrev.addEventListener('click', () => {
@@ -387,7 +459,12 @@ window.addEventListener('DOMContentLoaded', () => {
             offset -= +width.slice(0, width.length - 2);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
+
+        findSlide(sliderIndex);
     })
+
+
+
 
     // function insertIndex() {
     //     if (sliderIndex < 10) {
