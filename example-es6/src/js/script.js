b114b2dd2827f4d9f8e4d11d3a36d2bb15ee0f60
Дарья <dforst09@gmail.com>
2024-02-07 11:27:00 +0300

    add es6 import export example

---
+++ example-es6/src/js/script.js
@@ -0,0 +1,8 @@
+import { one, two } from './main.js';
+import sayHi from './main.js';
+
+console.log(`${one} and ${two}`);
+sayHi();
+
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
