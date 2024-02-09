8a77a537d30fe8c997be05d2a554b3c23d04ba76
Дарья <dforst09@gmail.com>
2024-02-09 13:15:07 +0300

    complete ex 22

--- ex 22/script.js
+++ ex 22/script.js
@@ -7,21 +7,40 @@ const multiply20 = (price) => price * 20;
 const divide100 = (price) => price / 100;
 const normalizePrice = (price) => price.toFixed(2);
 
-// const compose = (price, hehe, nothehe, he) => function (hihi, nothihi) {
-//     console.log(arguments);
-//     console.log(price, hehe, nothehe, he);
-//     console.log(typeof (price));
-//     console.log(hihi, nothihi);
-// }
-
 const compose = (...args) => (price) => {
-    args.reverse().forEach((item, i) => {
-        console.log(item(price));
-    })
+    let result;
+    args.reverse().forEach(item => {
+        if (result) {
+            item(result);
+            result = item(result);
+        }
+        else {
+            result = item(price);
+        }
+    });
+    return result;
 }
 
 const discount = compose(normalizePrice, divide100, multiply20);
 
-discount(200.0);
+discount(150.0);
 
-// console.log(discount);
+const add1 = function (a) { return a + 1 }
+const addAll3 = function (a, b, c) { return a + b + c }
+
+const composeWithArgs = (...args) => (...params) => {
+    let result;
+
+    args.forEach(item => {
+        if (result) {
+            params[0] = result;
+            result = item(...params);
+        }
+        else {
+            result = item(...params)
+        }
+    })
+    return result;
+}
+
+composeWithArgs(add1, addAll3)(1, 2, 3)
